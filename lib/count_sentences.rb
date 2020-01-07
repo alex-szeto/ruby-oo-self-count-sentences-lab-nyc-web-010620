06de4dbdde57b9fc3b0daed87b3496d763158c4d
Alex Szeto <alex.szeto.a@gmail.com>
2020-01-07 20:01:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.split("")[-1] == "."
+      return true
+    end
+    return false
   end
 
   def question?
-
+    if self.split("")[-1] == "?"
+      return true
+    end
+    return false
   end
 
   def exclamation?
-
+    if self.split("")[-1] == "!"
+      return true
+    end
+    return false
   end
 
   def count_sentences
-
+    previous = false
+    count = 0
+    self.split("").each{ |char|
+      if char == "." || char == "?" || char == "!" 
+        if previous == false
+          count += 1
+          previous = true
+        end
+      else
+        previous = false
+      end
+    }
+    return count
   end
 end
